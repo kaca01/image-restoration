74feeb1616df2295ca5a1ffbe65b13e2c75e49f7
savic-a <anastasijas557@gmail.com>
2023-07-02 21:15:23 +0200

    Added class for generator

--- back/main.py
+++ back/main.py
@@ -1,6 +1,6 @@
-from data_preparation.dependencies_and_data import preparation, get_lr_images, get_hr_images
+from data_preparation.dependencies_and_data import preparation, get_hr_images
 from data_preparation.image_preparation import prepare_images
-from generator.model import model, show_generated_images
+from models.generator import Generator
 
 preparation()
 
@@ -8,6 +8,7 @@ images = get_hr_images()
 
 prepare_images(images)
 
-generator = model()
-# generator.summary()
-show_generated_images(generator)
+generator = Generator()
+gen_model = generator.model()
+# gen_model.summary()
+generator.show_generated_images(gen_model)
